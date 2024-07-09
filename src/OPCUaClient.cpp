b25c703dc50648fef4fd1b95d2004293a60d9be5
peter_shaw <blackart0@163.com>
2024-07-09 22:22:38 +0800

    initialization

---
+++ src/OPCUaClient.cpp
@@ -0,0 +1,356 @@
+/////////////////////////////////////////////////////////////////////////////
+// Name:        OPCUaClient.cpp
+// Purpose:     OPCUaClient
+// Author:      xcz
+// Modified by:
+// Copyright:   (c) Wind
+// Licence:     MIT licence
+/////////////////////////////////////////////////////////////////////////////
+#include "OPCUaClient.h"
+
+wxIMPLEMENT_APP(MyApp);
+
+bool MyApp::OnInit()
+{
+	wxImage::AddHandler(new wxPNGHandler);
+	MyFrame* frame = new MyFrame();
+	frame->Show(true);
+	return true;
+}
+
+MyFrame::~MyFrame()
+{
+	if (m_Font) {
+		delete m_Font;
+	}
+	if (m_uaClient) {
+		UA_Client_disconnect(m_uaClient);
+		UA_Client_delete(m_uaClient);
+	}
+}
+
+MyFrame::MyFrame() : wxFrame(NULL, wxID_ANY, L"KEPServerEX6Client", wxDefaultPosition, wxSize(800, 600))
+{
+	SetIcon(wxICON(sample));
+	m_text = new wxTextCtrl(this, wxID_ANY, L"KEPServerEX6Client \n", wxDefaultPosition, wxDefaultSize, wxTE_MULTILINE | wxTE_READONLY);
+	m_text->SetMinSize(wxSize(300, 100));
+	delete wxLog::SetActiveTarget(new wxLogTextCtrl(m_text));
+	m_Font = new wxFont(m_iFontSize, wxFONTFAMILY_DEFAULT, wxFONTSTYLE_NORMAL, wxFONTWEIGHT_NORMAL, false, m_sFontName);
+
+	wxMenu* menuFile = new wxMenu;
+	menuFile->Append(ID_Menu_Hello, L"&Hello...\tCtrl-H", "Help string shown in status bar for this menu item");
+	menuFile->AppendSeparator();
+	menuFile->Append(wxID_EXIT);
+
+	wxMenu* menuHelp = new wxMenu;
+	menuHelp->Append(wxID_ABOUT);
+
+	wxMenuBar* menuBar = new wxMenuBar;
+	menuBar->Append(menuFile, L"&File");
+	menuBar->Append(menuHelp, L"&Help");
+	SetMenuBar(menuBar);
+
+	CreateStatusBar(4);
+	SetStatusText(wxT("Welcome to KEPServerEX6Client!"), 0);
+	SetStatusText(wxString("power by Open62541: ") + UA_OPEN62541_VERSION, 1);
+
+	wxBoxSizer* topsizer = new wxBoxSizer(wxVERTICAL);
+
+	wxNotebook* notebook = new wxNotebook(this, wxID_ANY);
+	notebook->SetFont(*m_Font);
+	topsizer->Add(notebook, 1, wxEXPAND | wxALL, 5);
+
+	wxButton* btnExit = new wxButton(this, wxID_EXIT, L"E&xit");
+	btnExit->SetFont(*m_Font);
+	wxBoxSizer* bSizer2 = new wxBoxSizer(wxHORIZONTAL);
+	bSizer2->Add(btnExit, 0, wxRIGHT, 5);
+
+	topsizer->Add(bSizer2, 0, wxALIGN_RIGHT | wxALL, 5);
+	topsizer->Add(m_text, 0, wxEXPAND | wxALL, 5);
+
+	// First page: -----------------------------------------------------------------------------
+	wxPanel* panel1 = new wxPanel(notebook, wxID_ANY);
+	panel1->SetMinSize(wxSize(1024, 400));
+	notebook->AddPage(panel1, L"OPC UA");
+	wxSizer* panelsizer1 = new wxBoxSizer(wxVERTICAL);
+	//* definition of IP Addresses text validator
+	wxString ipAddressFilter[11] = { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "." }; // authorized characters for IP Address
+	wxArrayString arraystrIPAddress(11, ipAddressFilter);
+	wxTextValidator txtvldIPAddress(wxFILTER_INCLUDE_CHAR_LIST); // text validator for IP Address
+	txtvldIPAddress.SetIncludes(arraystrIPAddress); // sets authorized characters for IP Address
+	// check of IP address
+	// regular expression for IP address (4 [0-255] decimal numbers separated by periods)
+	// [0-9]{1} : exactly 1 decimal digit ([0-9])
+	// |[0-9]{2} : ...or exactly 2 decimal digits ([10-99])
+	// |[0-1][0-9]{2} : ...or '0'/'1' followed by exactly 2 decimal digits ([000-199])
+	// |2[0-4][0-9] : ...or 2 followed by '0'/'1'/'2'/'3'/'4' followed by 1 decimal digit ([200-249])
+	// |25[0-5] : ...or '25' followed by '0'/'1'/'2'/'3'/'4'/'5' ([250-255])
+	// (([0-9]{1}|[0-9]{2}|[0-1][0-9]{2}|2[0-4][0-9]|25[0-5])\\.) : ...the whole followed by '.' (quoted)
+	// (([0-9]{1}|[0-9]{2}|[0-1][0-9]{2}|2[0-4][0-9]|25[0-5])\\.){3} : ...the whole exactly 3 times
+	// ^(([0-9]{1}|[0-9]{2}|[0-1][0-9]{2}|2[0-4][0-9]|25[0-5])\\.){3} : ...positioned at start
+	// ([0-9]{1}|[0-9]{2}|[0-1][0-9]{2}|2[0-4][0-9]|25[0-5]) : [0-9] + [10-99] + [000-199] + [200-249] + [250-255]
+	// ([0-9]{1}|[0-9]{2}|[0-1][0-9]{2}|2[0-4][0-9]|25[0-5])$ : ...positioned at the end
+	// wxRegEx regxIPAddr("^(([0-9]{1}|[0-9]{2}|[0-1][0-9]{2}|2[0-4][0-9]|25[0-5])\\.){3}([0-9]{1}|[0-9]{2}|[0-1][0-9]{2}|2[0-4][0-9]|25[0-5])$");
+	// if (!regxIPAddr.Matches(ipAddress)) {
+	//    ...
+	// }
+
+	//* drawing of GUI
+	m_textHost = new wxTextCtrl(panel1, wxID_ANY, L"127.0.0.1", wxDefaultPosition, wxSize(150, wxDefaultCoord), wxTE_CENTRE, txtvldIPAddress);
+	m_textHost->SetToolTip(L"Four [0-255] decimal numbers separated by periods\nFormat: A.B.C.D\nEx.: 192.168.1.1");
+	m_textPort = new wxTextCtrl(panel1, wxID_ANY, L"49320", wxDefaultPosition, wxSize(80, wxDefaultCoord), wxTE_CENTRE, txtvldIPAddress);
+	m_btnConnect = new wxButton(panel1, ID_Btn_Connect, L"Connect");
+	m_btnBrowse = new wxButton(panel1, ID_Btn_Browse, L"Browse");
+	m_btnGetValue = new wxButton(panel1, ID_Btn_GetValue, L"GetValue");
+	m_btnBrowse->Enable(false);
+	m_btnGetValue->Enable(false);
+	m_textHost->SetFont(*m_Font);
+	m_textPort->SetFont(*m_Font);
+	m_btnConnect->SetFont(*m_Font);
+	m_btnBrowse->SetFont(*m_Font);
+	m_btnGetValue->SetFont(*m_Font);
+
+	wxSizer* sizerRow = new wxBoxSizer(wxHORIZONTAL);
+	wxStaticText* stServer = new wxStaticText(panel1, wxID_ANY, L"Server: ");
+	wxStaticText* stPort = new wxStaticText(panel1, wxID_ANY, L"Port: ");
+	stServer->SetFont(*m_Font);
+	stPort->SetFont(*m_Font);
+	sizerRow->Add(stServer, 0, wxALIGN_CENTRE_VERTICAL | wxALL, 5);
+	sizerRow->Add(m_textHost, 0, wxALIGN_CENTRE_VERTICAL | wxLEFT, 0);
+	sizerRow->Add(stPort, 0, wxALIGN_CENTRE_VERTICAL | wxLEFT, 10);
+	sizerRow->Add(m_textPort, 0, wxALIGN_CENTRE_VERTICAL | wxLEFT, 5);
+	sizerRow->Add(m_btnConnect, 0, wxALIGN_CENTRE_VERTICAL | wxLEFT, 20);
+	sizerRow->Add(m_btnBrowse, 0, wxALIGN_CENTRE_VERTICAL | wxLEFT, 10);
+	sizerRow->Add(m_btnGetValue, 0, wxALIGN_CENTRE_VERTICAL | wxLEFT, 10);
+
+	wxSizer* sizerRow1 = new wxBoxSizer(wxHORIZONTAL);
+	//m_gridObjs
+	m_gridObjs = new wxGrid(panel1, wxID_ANY, wxDefaultPosition, wxDefaultSize);
+	m_gridObjs->SetFont(*m_Font);
+	m_gridObjs->SetDefaultCellFont(*m_Font);
+	m_gridObjs->CreateGrid(30, 20);
+	m_gridObjs->SetMinSize(wxSize(280, 520));
+	m_gridObjs->SetRowLabelSize(50);
+	m_gridObjs->SetLabelBackgroundColour(wxColour(0xe9e4c1));
+	m_gridObjs->EnableEditing(false);
+
+	//m_gridItems
+	m_gridItems = new wxGrid(panel1, wxID_ANY, wxDefaultPosition, wxDefaultSize);
+	m_gridItems->SetFont(*m_Font);
+	m_gridItems->SetDefaultCellFont(*m_Font);
+	m_gridItems->CreateGrid(30, 20);
+	m_gridItems->SetMinSize(wxSize(320, 100));
+	m_gridItems->SetRowLabelSize(50);
+	m_gridItems->SetLabelBackgroundColour(wxColour(0xe9e4c1));
+	m_gridItems->EnableEditing(false);
+
+	sizerRow1->Add(m_gridObjs, 0, wxEXPAND | wxALL, 10);
+	sizerRow1->Add(m_gridItems, 1, wxEXPAND | wxALL, 10);
+	panelsizer1->Add(sizerRow1, 1, wxEXPAND | wxALL, 10);
+	panelsizer1->Add(sizerRow, 0, wxEXPAND | wxALL, 10);
+	panel1->SetSizer(panelsizer1);
+
+	// Second page: a text ctrl and a button
+	wxPanel* panel2 = new wxPanel(notebook, wxID_ANY);
+	notebook->AddPage(panel2, L"OPC DA");
+	wxSizer* panelsizer2 = new wxBoxSizer(wxVERTICAL);
+	panelsizer2->Add(new wxStaticText(panel2, wxID_ANY, L"开发中 ... ..."), 0, wxALIGN_CENTRE | wxLEFT, 10);
+	//wxTextCtrl* text = new wxTextCtrl(panel, wxID_ANY, "TextLine 1.", wxDefaultPosition, wxSize(250, wxDefaultCoord));
+	//panelsizer->Add(text, 0, wxEXPAND | wxALL, 10);
+	//text = new wxTextCtrl(panel, wxID_ANY, "TextLine 2.", wxDefaultPosition, wxSize(250, wxDefaultCoord));
+	//panelsizer->Add(text, 0, wxEXPAND | wxALL, 10);
+	//wxButton* button2 = new wxButton(panel, wxID_ANY, "Hallo");
+	//panelsizer2->Add(button2, 0, wxEXPAND | wxALL, 0);
+	panel2->SetSizer(panelsizer2);
+
+	SetSizerAndFit(topsizer);
+	Layout();
+	Center();
+
+	Bind(wxEVT_MENU, &MyFrame::OnHello, this, ID_Menu_Hello);
+	Bind(wxEVT_MENU, &MyFrame::OnAbout, this, wxID_ABOUT);
+	Bind(wxEVT_MENU, &MyFrame::OnExit, this, wxID_EXIT);
+
+	Bind(wxEVT_BUTTON, &MyFrame::OnBtnConnect, this, ID_Btn_Connect);
+	Bind(wxEVT_BUTTON, &MyFrame::OnBtnBrowse, this, ID_Btn_Browse);
+	Bind(wxEVT_BUTTON, &MyFrame::OnBtnGetValue, this, ID_Btn_GetValue);
+	Bind(wxEVT_BUTTON, &MyFrame::OnExit, this, wxID_EXIT);
+}
+
+void MyFrame::OnExit(wxCommandEvent& event)
+{
+	Close(true);
+}
+
+void MyFrame::OnAbout(wxCommandEvent& event)
+{
+	wxMessageBox(L"This is a wxWidgets Hello World example!", L"About Hello World", wxOK | wxICON_INFORMATION);
+}
+
+void MyFrame::OnHello(wxCommandEvent& event)
+{
+	wxLogMessage(L"Hello world from wxWidgets!");
+}
+
+void MyFrame::OnBtnConnect(wxCommandEvent& event)
+{
+	if (m_uaClient) {
+		m_btnBrowse->Enable(false);
+		m_btnGetValue->Enable(false);
+		UA_Client_delete(m_uaClient); /* Disconnects the client internally */
+	}
+	m_uaClient = UA_Client_new();
+	UA_ClientConfig_setDefault(UA_Client_getConfig(m_uaClient));
+
+	m_sHost = m_textHost->GetValue();
+	m_sPort = m_textPort->GetValue();
+	wxString url = wxString::Format(L"opc.tcp://%s:%s", m_sHost, m_sPort);
+	UA_StatusCode status = UA_Client_connect(m_uaClient, url.c_str());
+	if (status != UA_STATUSCODE_GOOD) {
+		//UA_LOG_INFO(UA_Log_Stdout, UA_LOGCATEGORY_USERLAND, "The connection failed with status code %s", UA_StatusCode_name(status));
+		wxLogMessage(wxString::Format(L"UA_Client_connect opc.tcp://%s:%s failed with status code %s", m_sHost, m_sPort, UA_StatusCode_name(status)));
+		UA_Client_delete(m_uaClient);
+		return;
+	}
+	else {
+		m_btnBrowse->Enable();
+		m_btnGetValue->Enable();
+		wxLogMessage(wxString::Format(L"UA_Client_connect opc.tcp://%s:%s successed!", m_sHost, m_sPort));
+	}
+}
+
+void MyFrame::OnBtnBrowse(wxCommandEvent& event)
+{
+	//Browse_nodes(m_uaClient, UA_NODEID_NUMERIC(0, UA_NS0ID_SERVER));
+	//Browse_nodes(m_uaClient, UA_NODEID_NUMERIC(0, UA_NS0ID_OBJECTSFOLDER));
+	char sNodeID[1024] = "xpy.DK";
+	Browse_nodes(m_uaClient, UA_NODEID_STRING(2, sNodeID));
+}
+void MyFrame::Browse_nodes(UA_Client* client, UA_NodeId nodeId)
+{
+	UA_BrowseRequest bReq;
+	UA_BrowseRequest_init(&bReq); // 初始化bReq
+	bReq.requestedMaxReferencesPerNode = 0; // 限制查到的最大节点数，0 不限制
+	//UA_BROWSEDIRECTION_FORWARD表示向下查找（即查找添加在节点下的节点），
+	//UA_BROWSEDIRECTION_INVERSE表示向上查找（即查找节点的父节点），
+	//UA_BROWSEDIRECTION_BOTH表示上下都进行查找
+	bReq.nodesToBrowse = UA_BrowseDescription_new(); // 分配内存，并把地址赋给bReq.nodesToBrowse
+	bReq.nodesToBrowse[0].browseDirection = UA_BROWSEDIRECTION_FORWARD;
+	bReq.nodesToBrowse[0].includeSubtypes = UA_TRUE;//是否包含subtypes
+
+	bReq.nodesToBrowseSize = 1; // 遍历的起始节点数量
+	bReq.nodesToBrowse[0].nodeId = nodeId; // 遍历 nodeId 下的目录
+	bReq.nodesToBrowse[0].nodeClassMask = UA_NODECLASS_OBJECT | UA_NODECLASS_VARIABLE | UA_NODECLASS_METHOD; // 只查看对象，变量和方法节点
+	bReq.nodesToBrowse[0].resultMask = UA_BROWSERESULTMASK_ALL; //返回浏览到的节点包含的信息，名称、显示名称......，UA_BROWSERESULTMASK_ALL表示返回所有信息
+
+
+	int rows = m_gridObjs->GetNumberRows();
+	int cols = m_gridObjs->GetNumberCols();
+	if (rows > 0) {
+		m_gridObjs->DeleteRows(0, rows);
+	}
+	if (cols > 0) {
+		m_gridObjs->DeleteCols(0, cols);
+	}
+
+	UA_BrowseResponse bResp = UA_Client_Service_browse(m_uaClient, bReq); // 开始遍历
+	if (bResp.responseHeader.serviceResult == UA_STATUSCODE_GOOD) // 判断遍历结果
+	{
+		m_gridObjs->AppendCols(3);
+		m_gridObjs->SetColLabelValue(0, L"Index");
+		m_gridObjs->SetColLabelValue(1, L"nodeID");
+		m_gridObjs->SetColLabelValue(2, L"BROWSE NAME");
+		m_gridObjs->SetColSize(0, 30);
+		m_gridObjs->SetColSize(1, 110);
+		m_gridObjs->SetColSize(2, 80);
+		//m_gridObjs->HideCol(1);
+		// 打印遍历结果
+		char s1[1024] = { "\0" };
+		char s2[1024] = { "\0" };
+		for (size_t i = 0; i < bResp.resultsSize; ++i) {
+			for (size_t j = 0; j < bResp.results[i].referencesSize; ++j) {
+				UA_ReferenceDescription* ref = &(bResp.results[i].references[j]);
+				m_gridObjs->AppendRows();
+				if (ref->nodeId.nodeId.identifierType == UA_NODEIDTYPE_NUMERIC) {
+					memset(s1, 0, 1024);
+					memcpy_s(s1, 1024, ref->browseName.name.data, (int)ref->browseName.name.length);
+					m_gridObjs->SetCellValue(j, 0, wxString::Format(L"%d", (int)ref->nodeId.nodeId.namespaceIndex));
+					m_gridObjs->SetCellValue(j, 1, wxString::Format(L"%d", (int)ref->nodeId.nodeId.identifier.numeric));
+					m_gridObjs->SetCellValue(j, 2, wxString::FromUTF8(s1));
+				}
+				else if (ref->nodeId.nodeId.identifierType == UA_NODEIDTYPE_STRING) {
+					memset(s1, 0, 1024);
+					memset(s2, 0, 1024);
+					memcpy_s(s1, 1024, ref->nodeId.nodeId.identifier.string.data, (int)ref->nodeId.nodeId.identifier.string.length);
+					memcpy_s(s2, 1024, ref->browseName.name.data, (int)ref->browseName.name.length);
+
+					m_gridObjs->SetCellValue(j, 0, wxString::Format(L"%d", ref->nodeId.nodeId.namespaceIndex));
+					m_gridObjs->SetCellValue(j, 1, wxString::FromUTF8(s1));
+					m_gridObjs->SetCellValue(j, 2, wxString::FromUTF8(s2));
+				}
+			}
+		}
+	}
+
+	UA_BrowseResponse_clear(&bResp);
+	//UA_BrowseRequest_clear(&bReq);
+}
+void MyFrame::OnBtnGetValue(wxCommandEvent& event)
+{
+	/* Read the value attribute of the node. UA_Client_readValueAttribute is a
+	 * wrapper for the raw read service available as UA_Client_Service_read. */
+	UA_StatusCode status;
+	UA_Variant value; /* Variants can hold scalar values and arrays of any type */
+	UA_Variant_init(&value);
+	char node_name_cc1[] = "XPY.DK.R0101";
+	status = UA_Client_readValueAttribute(m_uaClient, UA_NODEID_STRING(2, node_name_cc1), &value);
+	if (status == UA_STATUSCODE_GOOD) {
+		if (UA_Variant_hasScalarType(&value, &UA_TYPES[UA_TYPES_INT16])) {
+			wxLogMessage(wxString::Format(L"the \"XPY.DK.R0101\" value is: %d", *(UA_Int16*)value.data));
+		}
+		else {
+			wxLogMessage(L"XPY.DK.R0101 获取到数据，但数据类型不匹配");
+		}
+	}
+	else {
+		wxLogMessage(L"XPY.DK.R101 未取到数据");
+	}
+
+	char node_name_cc2[] = "_System._DateTimeLocal";
+	status = UA_Client_readValueAttribute(m_uaClient, UA_NODEID_STRING(2, node_name_cc2), &value);
+	if (status == UA_STATUSCODE_GOOD) {
+		if (UA_Variant_hasScalarType(&value, &UA_TYPES[UA_TYPES_DATETIME])) {
+			UA_DateTimeStruct dts = UA_DateTime_toStruct(*(UA_DateTime*)value.data);
+			wxLogMessage(wxString::Format(L"the \"_System._DateTimeLocal\" is: %04u.%02u.%02u %02u:%02u:%02u.%03u", dts.year, dts.month, dts.day, dts.hour, dts.min, dts.sec, dts.milliSec));
+		}
+		else {
+			wxLogMessage(L"_System._DateTimeLocal 获取到数据，但数据类型不匹配");
+		}
+	}
+	else {
+		wxLogMessage(L"_System._DateTimeLocal 未取到数据");
+	}
+
+	/* Clean up */
+	UA_Variant_clear(&value);
+
+}
+//
+//
+//// 写操作
+//void OPC_api::OPC_writeValue(const char* node_name_cc)
+//{
+//	char* node_name = const_cast <char*>(node_name_cc);
+//	OPC_test = OPC_test + 100;
+//	UA_Variant_setScalar(&value, &OPC_test, &UA_TYPES[UA_TYPES_UINT32]);
+//	status = UA_Client_writeValueAttribute(client, UA_NODEID_STRING(2, node_name), &value);
+//	if (status != UA_STATUSCODE_GOOD)
+//	{
+//		cout << "write Failed" << endl;
+//	}
+//	else
+//	{
+//		cout << "write Successful" << endl;
+//	}
+//	cout << "OPC_test更改为 :" << OPC_test << endl;
+//}
---
+++ src/OPCUaClient.h
@@ -0,0 +1,73 @@
+/////////////////////////////////////////////////////////////////////////////
+// Name:        OPCUaClient.h
+// Purpose:     OPCUaClient
+// Author:      xcz
+// Modified by:
+// Copyright:   (c) Wind
+// Licence:     MIT licence
+/////////////////////////////////////////////////////////////////////////////
+#pragma once
+#ifndef _OPCUaClient_H_
+#define _OPCUaClient_H_
+
+#include <wx/wxprec.h>
+#ifndef WX_PRECOMP
+#include <wx/wx.h>
+#endif
+
+#include <wx/event.h>
+#include <wx/image.h>
+#include <wx/graphics.h>
+#include <wx/grid.h>
+#include <wx/notebook.h>
+
+#include "open62541.h"
+
+class MyApp : public wxApp
+{
+public:
+    virtual bool OnInit();
+};
+
+class MyFrame : public wxFrame
+{
+public:
+    MyFrame();
+    ~MyFrame();
+private:
+    void OnHello(wxCommandEvent& event);
+    void OnExit(wxCommandEvent& event);
+    void OnAbout(wxCommandEvent& event);
+    void OnBtnConnect(wxCommandEvent& event);
+    void OnBtnBrowse(wxCommandEvent& event);
+    void OnBtnGetValue(wxCommandEvent& event);
+
+    void Browse_nodes(UA_Client* client, UA_NodeId nodeId);
+
+    UA_Client* m_uaClient = nullptr;
+    int m_iFontSize = 9;
+    wxString m_sHost = "";
+    wxString m_sPort = "";
+    wxString m_sFontName = L"微软雅黑";
+    wxLog* m_log = nullptr;
+    wxFont* m_Font = nullptr;
+    wxGrid* m_gridObjs = nullptr;
+    wxGrid* m_gridItems = nullptr;
+    wxButton* m_btnConnect = nullptr;
+    wxButton* m_btnBrowse = nullptr;
+    wxButton* m_btnGetValue = nullptr;
+    wxTextCtrl* m_text = nullptr;
+    wxTextCtrl* m_textHost = nullptr;
+    wxTextCtrl* m_textPort = nullptr;
+};
+
+enum enum_EVENTID
+{
+    ID_Menu_Hello = 1,
+    ID_Btn_Connect,
+    ID_Btn_Disconnect,
+    ID_Btn_Browse,
+    ID_Btn_GetValue,
+};
+
+#endif
